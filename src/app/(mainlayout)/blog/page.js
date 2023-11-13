4b7959ed48e0b2800113f6c33260e786fb8693ea
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-11-13 20:54:11 +0600

    update work

--- src/app/(mainlayout)/blog/page.js
+++ src/app/(mainlayout)/blog/page.js
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import { BiArrowToLeft, BiArrowToRight } from "react-icons/bi";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
+import Image from "next/image";
 
 const Blog = () => {
   const [page, setPage] = useState(1);
@@ -23,12 +24,8 @@ const Blog = () => {
         }}
       >
         <div className="text-white max-w-fit">
-          <h1 className="text-6xl font-semibold uppercase">Blog</h1>
-          <div className="flex items-center justify-between text-lg">
-            <Link href={"/home"}>Home</Link>
-            <BiArrowToRight />
-            <Link href={"/blog"}>Blog</Link>
-          </div>
+          <h1 className="text-6xl font-semibold uppercase text-center mb-1">Blog</h1>
+          <p className="text-center">Don&apos;t focus on having a great blog. Focus on producing a blog that&apos;s great for your readers.</p>
         </div>
       </div>
       {/* blog cover end */}
@@ -39,24 +36,46 @@ const Blog = () => {
           {/* <div className="grid grid-cols-1 col-span-3 gap-6 sm:grid-cols-2 lg:grid-cols-3"> */}
           <div className="grid grid-cols-1 col-span-3 gap-4 md:grid-cols-2 xl:grid-cols-3">
             {[1, 2, 3, 4, 5, 6, 7, 8].map((e, i) => (
-              <div
-                className="w-full overflow-hidden bg-white shadow-lg cursor-pointer "
+              <article
+                class="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm"
                 key={i}
               >
-                <img
-                  className="object-cover w-full transition-transform duration-500 transform hover:scale-110 "
-                  src="/Assets/destination-5.jpg"
-                  alt=""
+                <Image
+                  alt="Office"
+                  src="https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  class="h-48 w-full object-cover transition-transform duration-500 transform hover:scale-110"
+                  width={800}
+                  height={300}
                 />
-                <div className="p-4 space-y-2">
-                  <h3 className="text-sm font-normal uppercase sm:text-base text-lime-600">
-                    Admin | Tours & Travels
+
+                <div class="p-4 sm:p-6">
+                  <h3 class="text-lg font-medium text-gray-900">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   </h3>
-                  <h1 className="text-xl sm:text-3xl hover:text-lime-600">
-                    Cox's Bazar such a beautiful place look like
-                  </h1>
+
+                  <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+                    dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
+                    sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
+                    voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
+                    Molestias explicabo corporis voluptatem?
+                  </p>
+
+                  <Link
+                    href={`/blogdetails/${1}`}
+                    class="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-lime-600"
+                  >
+                    Find out more
+
+                    <span
+                      aria-hidden="true"
+                      class="block transition-all group-hover:ms-0.5 rtl:rotate-180"
+                    >
+                      &rarr;
+                    </span>
+                  </Link>
                 </div>
-              </div>
+              </article>
             ))}
           </div>
           {/* blog cards end */}
@@ -98,12 +117,14 @@ const Blog = () => {
                     key={i}
                     className="h-[100px] cursor-pointer overflow-hidden flex items-center gap-2 bg-white shadow-lg"
                   >
-                    <img
-                      className="w-[120px] transition-transform duration-500 transform hover:scale-110 object-cover h-full"
+                    <Image
+                      className="w-[90px] transition-transform duration-500 transform hover:scale-110 object-cover h-52"
                       src="/Assets/blog/blog-2.jpeg"
                       alt=""
+                      width={500}
+                      height={500}
                     />
-                    <div className="p-2 space-y-1">
+                    <div className="py-5 px-1 space-y-1">
                       <h1 className="hover:text-lime-600">
                         Sundarban awesome travel guide
                       </h1>
