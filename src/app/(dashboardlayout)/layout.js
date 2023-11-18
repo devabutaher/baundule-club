d82ad7cacd724a7bb769fbe218165e444080969d
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-11-18 23:25:29 +0600

    update work

--- src/app/(dashboardlayout)/layout.js
+++ src/app/(dashboardlayout)/layout.js
@@ -14,7 +14,7 @@ const MainLayout = ({ children }) => {
             <Toaster />
             <QueryClientProvider client={queryClient}>
                 <UserContext>
-                    <div className='min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-700 text-white'>
+                    <div className='min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased'>
                         <DashHeader />
                         <Sidebar />
                         <div className='ml-14 md:ml-64'>
